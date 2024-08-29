2ee341c32c065c7f69237e2a91d8c1700a042be1
Sergio-Daniel-Pires <sergiodanpires@gmail.com>
2024-08-29 00:05:56 -0300

    Add read_message decorator and comments for echo_bot

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -2,7 +2,8 @@ import asyncio
 import logging
 
 from whatsapp.bot import WhatsappBot, bot_options_parser
-from whatsapp.messages import Incoming, MessageTypes, ReadMessage, TextMessage
+from whatsapp.messages import Incoming, MessageTypes, TextMessage
+from whatsapp.utils import read_message
 
 # Enable logging
 logging.basicConfig(
@@ -13,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 START = range(1)
 
+@read_message
 async def echo (bot: WhatsappBot, incoming: Incoming) -> int:
     """
     Echo function to send the same message back to the user.
@@ -30,24 +32,25 @@ async def echo (bot: WhatsappBot, incoming: Incoming) -> int:
     try:
         await bot.send_message(echo_msg, incoming.metadata.phone_number_id)
 
-        read_msg = ReadMessage.to_send(incoming.message.id)
-        await bot.send_message(read_msg, incoming.metadata.phone_number_id)
-
     except Exception as exc:
         logger.error(f"Error sending message: {exc}")
 
     return START
 
 async def main (args: list[str] = None):
+    # Parse args from terminal, like "python echo_bot.py --phone_number_id=1234567890"
     user_args = bot_options_parser.parse_args(args)
     user_args_as_kwargs = dict(user_args._get_kwargs())
 
     bot: WhatsappBot = WhatsappBot.from_dict(user_args_as_kwargs)
 
+    # Add a new state to the bot, that captures all text messages
     bot.add_new_state(START, echo, MessageTypes.TEXT)
 
+    # Start webhook server in a separeted Thread
     bot.start_webhook()
 
+    # Verify updates in shared queue
     await bot.run_forever()
 
 if __name__ == "__main__":
